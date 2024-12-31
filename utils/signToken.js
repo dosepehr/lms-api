c8c669f2d6a236d96a234306b1c8f4add0095f4d
dosepehr <dosepehr@gmail.com>
2024-12-31 13:49:26 +0330

    edit token utils

--- utils/signToken.js
+++ utils/signToken.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-
+require('dotenv').config();
 const secretKey = process.env.JWT_SECRET;
 const expiresIn = process.env.JWT_EXPIRES;
 
--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -1,8 +1,9 @@
 const { promisify } = require('util');
+require('dotenv').config();
+
 const jwt = require('jsonwebtoken');
 
 const secretKey = process.env.JWT_SECRET;
-
 const verifyToken = async (token) =>
     await promisify(jwt.verify)(token, secretKey);
 
