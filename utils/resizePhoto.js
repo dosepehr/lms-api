a09c8b73d788fe3567ff139b7f7d332966591aa0
dosepehr <dosepehr@gmail.com>
2025-01-02 00:21:07 +0330

    add resizing photos

---
+++ utils/resizePhoto.js
@@ -0,0 +1,34 @@
+const expressAsyncHandler = require('express-async-handler');
+const sharp = require('sharp');
+
+exports.resizePhoto = expressAsyncHandler(async (req, res, next) => {
+    if (!req.file) return next();
+    req.file.filename = `${Date.now()}-${req.user.id}.png`;
+    await sharp(req.file.buffer)
+        .resize(500, 500)
+        .toFormat('png')
+        .png({
+            quality: 90,
+        })
+        .toFile(`public/uploads/${req.file.filename}`);
+    req.body.cover = req.file.filename;
+    next();
+});
+exports.resizePhotos = expressAsyncHandler(async (req, res, next) => {
+    if (!req.files) return next();
+
+    req.body.images = [];
+    // can use Promise.all too
+    req.files.map(async (file, i) => {
+        const filename = `tour-${req.params.id}-${Date.now()}-${i + 1}.png`;
+        req.body.images.push(filename);
+        await sharp(file.buffer)
+            .resize(500, 500)
+            .toFormat('png')
+            .png({
+                quality: 90,
+            })
+            .toFile(`public/uploads/${filename}`);
+    }),
+        next();
+});
