6126d5e6a983e86132e12031e3cb250a9d0c73f7
dosepehr <dosepehr@gmail.com>
2025-01-01 11:24:15 +0330

    add reading routes

--- modules/Category/categoryController.js
+++ modules/Category/categoryController.js
@@ -1,6 +1,9 @@
 const Category = require('./categoryModel');
 const { addCategorySchema } = require('./categoryValidator');
-const { addOne, deleteOne } = require('../Factory/factoryController');
+const { addOne, deleteOne, getAll, getOne } = require('../Factory/factoryController');
 
 exports.addCategory = addOne(Category, addCategorySchema);
 exports.deleteCategory = deleteOne(Category);
+
+exports.getCategories = getAll(Category);
+exports.getCategory = getOne(Category);
--- modules/Category/categoryRouter.js
+++ modules/Category/categoryRouter.js
@@ -1,11 +1,20 @@
 const express = require('express');
-const { addCategory, deleteCategory } = require('./categoryController');
+const {
+    addCategory,
+    deleteCategory,
+    getCategory,
+    getCategories,
+} = require('./categoryController');
 const { protect, restrictTo } = require('../Auth/authController');
 
 const categoryRouter = express.Router();
 
-categoryRouter.route('/').post(protect, restrictTo('admin'), addCategory);
+categoryRouter
+    .route('/')
+    .get(getCategories)
+    .post(protect, restrictTo('admin'), addCategory);
 categoryRouter
     .route('/:id')
+    .get(getCategory)
     .delete(protect, restrictTo('admin'), deleteCategory);
 module.exports = categoryRouter;
