24a7df037718c50ab33b6db97539cd37c2308bca
dosepehr <dosepehr@gmail.com>
2025-01-01 11:10:32 +0330

    add factory function

--- modules/Category/categoryController.js
+++ modules/Category/categoryController.js
@@ -1,12 +1,5 @@
-const expressAsyncHandler = require('express-async-handler');
 const Category = require('./categoryModel');
 const { addCategorySchema } = require('./categoryValidator');
+const { addOne } = require('../Factory/factoryController');
 
-exports.addCategory = expressAsyncHandler(async (req, res, next) => {
-    await addCategorySchema.validate(req.body);
-    await Category.create(req.body);
-    res.status(201).json({
-        status: true,
-        messege: 'category created',
-    });
-});
+exports.addCategory = addOne(Category, addCategorySchema);
