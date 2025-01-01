3bcaf793449a9711e2ffe62712bc4f770fc49707
dosepehr <dosepehr@gmail.com>
2025-01-01 11:00:58 +0330

    edit categoryModel

--- modules/Category/categoryModel.js
+++ modules/Category/categoryModel.js
@@ -4,10 +4,14 @@ const categorySchema = new mongoose.Schema(
         title: {
             type: String,
             required: true,
+            unique: true,
         },
         slug: {
             type: String,
             required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
         },
     },
     {
