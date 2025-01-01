bc5c849dabe45d98b5c3246bbca31a7e0ae6c4e2
dosepehr <dosepehr@gmail.com>
2025-01-02 00:42:35 +0330

    edit session validation

--- modules/Session/sessionValidator.js
+++ modules/Session/sessionValidator.js
@@ -8,6 +8,9 @@ const addSessionSchema = yup.object().shape({
         .max(50, 'The title must be at most 50 characters long.'),
     videoUrl: yup.string().required('The Video is required'),
     course: yup.string().required('The course is required'),
+    videoDuration: yup
+        .number('The videoDuration must be a number')
+        .required('The videoDuration is required'),
 });
 const updateSessionSchema = yup.object().shape({
     title: yup
@@ -17,6 +20,9 @@ const updateSessionSchema = yup.object().shape({
         .max(50, 'The title must be at most 50 characters long.'),
     videoUrl: yup.string().required('The video is required'),
     course: yup.string().required('The course is required'),
+    videoDuration: yup
+        .number('The videoDuration must be a number')
+        .required('The videoDuration is required'),
 });
 
 module.exports = {
