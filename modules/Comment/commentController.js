ccacb723cb1b5923ed0204ffb3671df81b2e7a86
dosepehr <dosepehr@gmail.com>
2025-01-04 12:14:18 +0330

    add chnaging comment status

--- modules/Comment/commentController.js
+++ modules/Comment/commentController.js
@@ -1,3 +1,4 @@
+const expressAsyncHandler = require('express-async-handler');
 const { addOne, getAll } = require('../Factory/factoryController');
 const Comment = require('./CommentModel');
 
@@ -12,3 +13,20 @@ exports.getComments = getAll(Comment, {}, [
         select: 'username',
     },
 ]);
+
+exports.changeCommentStatus = expressAsyncHandler(async (req, res, next) => {
+    const { commentId } = req.params;
+    const comment = await Comment.findById(commentId);
+    if (!comment) {
+        res.status(404).json({
+            status: false,
+            message: 'No comment found',
+        });
+    }
+    comment.isAccept = comment.isAccept ? 0 : 1;
+    comment.save();
+    res.status(200).json({
+        status: true,
+        message: `comment ${comment.isAccept ? 'accepted' : 'rejected'}`,
+    });
+});
--- modules/Comment/commentRouter.js
+++ modules/Comment/commentRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { addComment, getComments } = require('./commentController');
+const { addComment, getComments, changeCommentStatus } = require('./commentController');
 const { protect, restrictTo } = require('../Auth/authController');
 
 const commentRouter = express.Router();
 
 commentRouter.route('/').post(protect, addComment).get(getComments);
+commentRouter.route('/changeStatus/:commentId').put(changeCommentStatus);
 
 module.exports = commentRouter;
