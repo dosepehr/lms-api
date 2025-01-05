f7a5b2c1fc4925ca160208e3e9c548eb78ce695a
dosepehr <dosepehr@gmail.com>
2025-01-05 11:03:32 +0330

    add get user basket by admin

--- modules/Basket/basketController.js
+++ modules/Basket/basketController.js
@@ -1,7 +1,8 @@
 const expressAsyncHandler = require('express-async-handler');
 const Basket = require('./BasketModel');
 const AppError = require('../../utils/AppError');
-
+const User = require('../User/userModel');
+const formatPhoneNumber = require('../../utils/formatPhoneNumber');
 exports.addBasket = expressAsyncHandler(async (req, res, next) => {
     const { user } = req;
     const { course } = req.body;
@@ -24,5 +25,27 @@ exports.addBasket = expressAsyncHandler(async (req, res, next) => {
     res.status(201).json({
         message: 'course added to basket',
         status: true,
+    });
+});
+
+exports.getUserBasket = expressAsyncHandler(async (req, res, next) => {
+    const { phone } = req.params;
+    const user = await User.findOne({
+        phone: formatPhoneNumber(phone),
+    });
+    if (!user) {
+        return next(new AppError('no user found with this phone number', 404));
+    }
+    const userBasket = await Basket.findOne({
+        user: user._id,
+    })
+        .populate('user')
+        .populate('courses'); 
+    if (!userBasket) {
+        return next(new AppError('this user has no active basket', 404));
+    }
+    res.status(200).json({
+        status: true,
+        basekt: userBasket,
     });
 });
--- modules/Basket/basketRouter.js
+++ modules/Basket/basketRouter.js
@@ -1,8 +1,14 @@
 const express = require('express');
-const { addBasket } = require('./basketController');
+const { addBasket, getUserBasket } = require('./basketController');
 const { protect, restrictTo } = require('../Auth/authController');
 
 const basketRouter = express.Router();
 
 basketRouter.post('/add', protect, addBasket);
+basketRouter.get(
+    '/getUserBasket/:phone',
+    protect,
+    restrictTo('admin'),
+    getUserBasket,
+);
 module.exports = basketRouter;
